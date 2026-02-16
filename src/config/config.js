b4a8822c65a988e059f1468167bb3f9d4b93f724
Kartik Mishra <kartik26nov2004@gmail.com>
2026-02-16 16:13:27 +0530

    cookie config update

--- src/config/config.js
+++ src/config/config.js
@@ -43,6 +43,7 @@ const config = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.NODE_ENV === 'production' ? "none" : "lax",
+    domain: ".onrender.com",
     path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   }
